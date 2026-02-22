b1934e4c8a7869a2a516ebb5eaffffff37f6fef4
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2026-02-22 12:02:53 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -6,6 +6,9 @@ const config = {
 		adapter: adapter(),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		},
+		alias: {
+			$research: './research/data'
 		}
 	}
 };
