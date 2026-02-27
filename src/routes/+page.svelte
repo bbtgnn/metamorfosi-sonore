b4153cc8480e98ef9663d62e3abf29ce4b09de55
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2026-02-27 20:26:29 +0100

    improve layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { resolve } from '$app/paths';
+	import Logo from '$lib/assets/logo.svg';
 
 	import AcciaioPreview from './posters/i-colori-dell-acciaio/preview.jpg';
 	import LaminatorePreview from './posters/il-laminatore/preview.jpg';
@@ -12,17 +13,26 @@
 	type Routes = Parameters<typeof resolve>[0];
 </script>
 
-<div class="grid grid-cols-1 gap-3 p-3 md:grid-cols-2 lg:grid-cols-4">
-	{@render image(AcciaioPreview, "I colori dell'acciaio", '/posters/i-colori-dell-acciaio')}
-	{@render image(LaminatorePreview, 'Il laminatore', '/posters/il-laminatore')}
-	{@render image(RisveglioPreview, 'Il risveglio', '/posters/il-risveglio')}
-	{@render image(OperaioIgnotoPreview, 'Operaio ignoto', '/posters/operaio-ignoto')}
+<div class="max-w-screen-7xl mx-auto flex min-h-screen flex-col items-center justify-center">
+	<div class=" gap-4">
+		<div class="w-full border-b border-b-foreground p-2 md:p-4">
+			<img src={Logo} alt="Logo" class="h-32" />
+		</div>
+		<div class="inline-grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+			{@render image(AcciaioPreview, "I colori dell'acciaio", '/posters/i-colori-dell-acciaio')}
+			{@render image(LaminatorePreview, 'Il laminatore', '/posters/il-laminatore')}
+			{@render image(RisveglioPreview, 'Il risveglio', '/posters/il-risveglio')}
+			{@render image(OperaioIgnotoPreview, 'Operaio ignoto', '/posters/operaio-ignoto')}
+		</div>
+	</div>
 </div>
 
 {#snippet image(image: string, title: string, href: Routes)}
-	<a href={resolve(href)} class="self-center p-4 hover:bg-foreground hover:text-background">
+	<a href={resolve(href)} class="space-y-2 p-4 hover:bg-foreground hover:text-background">
 		<img src={image} alt={title} width={imageWidth} height={imageSize} />
-		{title}
+		<span>
+			{title}
+		</span>
 	</a>
 {/snippet}
 
@@ -30,7 +40,7 @@
 	@reference "./layout.css";
 
 	img {
-		@apply h-[400px] w-auto;
+		@apply max-h-[400px] w-auto;
 		aspect-ratio: attr(width) / attr(height);
 		object-fit: contain;
 		display: block;
