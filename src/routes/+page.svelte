88bbc02c4ef4da090cfef74ab0bd8af0878c103c
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2026-02-27 23:59:26 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { resolve } from '$app/paths';
 	import Logo from '$lib/assets/logo.svg';
-	import { imageSize } from '$lib/shared';
+	import { imageSize } from '$lib/canvas/utils';
 
 	import AcciaioPreview from './posters/i-colori-dell-acciaio/preview.jpg';
 	import LaminatorePreview from './posters/il-laminatore/preview.jpg';
@@ -11,22 +11,22 @@
 	type Routes = Parameters<typeof resolve>[0];
 </script>
 
-<div class="max-w-screen-7xl mx-auto flex min-h-screen flex-col items-center justify-center">
-	<div class=" gap-4">
-		<div class="w-full border-b border-b-foreground p-2 md:p-4">
-			<img src={Logo} alt="Logo" class="h-32" />
+<div class="flex min-h-screen flex-col items-stretch justify-center">
+	<div class="border-b border-b-foreground p-4">
+		<div class="max-w-screen-4xl mx-auto w-full">
+			<img src={Logo} alt="Logo" class="h-20 sm:h-32" />
 		</div>
-		<div class="inline-grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-			{@render image(AcciaioPreview, "I colori dell'acciaio", '/posters/i-colori-dell-acciaio')}
-			{@render image(LaminatorePreview, 'Il laminatore', '/posters/il-laminatore')}
-			{@render image(RisveglioPreview, 'Il risveglio', '/posters/il-risveglio')}
-			{@render image(OperaioIgnotoPreview, 'Operaio ignoto', '/posters/operaio-ignoto')}
-		</div>
+	</div>
+	<div class="max-w-screen-7xl mx-auto inline-grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+		{@render image(AcciaioPreview, "I colori dell'acciaio", '/posters/i-colori-dell-acciaio')}
+		{@render image(LaminatorePreview, 'Il laminatore', '/posters/il-laminatore')}
+		{@render image(RisveglioPreview, 'Il risveglio', '/posters/il-risveglio')}
+		{@render image(OperaioIgnotoPreview, 'Operaio ignoto', '/posters/operaio-ignoto')}
 	</div>
 </div>
 
 {#snippet image(image: string, title: string, href: Routes)}
-	<a href={resolve(href)} class="space-y-2 p-4 hover:bg-foreground hover:text-background">
+	<a href={resolve(href)} class="space-y-2 p-2 hover:bg-foreground hover:text-background md:p-4">
 		<img src={image} alt={title} width={imageSize.width} height={imageSize.height} />
 		<span>
 			{title}
